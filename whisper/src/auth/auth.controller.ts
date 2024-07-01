19280ead1e25b68ad7e35303e4cc7e4f3f445ba6
Rifandye <rilfransyah@gmail.com>
2024-07-01 22:15:58 +0700

    feat: add register

--- whisper/src/auth/auth.controller.ts
+++ whisper/src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  register(@Body() createUserDto: CreateUserDto): Promise<void> {
+    return this.authService.createUser(createUserDto);
+  }
 }
--- whisper/src/auth/auth.service.ts
+++ whisper/src/auth/auth.service.ts
@@ -1,4 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<void> {
+    try {
+      const { username, email, password } = createUserDto;
+
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const user = this.userRepository.create({
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username or Email already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+      console.log(error.code);
+    }
+  }
+}
