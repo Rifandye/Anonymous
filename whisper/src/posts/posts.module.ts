b39b4d428d393d9be0b41c98966a24329a721ec3
Rifandye <rilfransyah@gmail.com>
2024-07-03 23:54:43 +0700

    fix: fixing the deletePostById

--- whisper/src/posts/posts.module.ts
+++ whisper/src/posts/posts.module.ts
@@ -4,9 +4,10 @@ import { PostsService } from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './posts.entity';
 import { AuthModule } from '../auth/auth.module';
+import { Comment } from '../comments/comments.entitry';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Post, Comment]), AuthModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
