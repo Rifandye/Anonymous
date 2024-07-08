17df172c8969f6984b275f748253898ed4d56401
Rifandye <rilfransyah@gmail.com>
2024-07-08 17:54:47 +0700

    feat: add like to post

--- whisper/src/likes/likes.controller.ts
+++ whisper/src/likes/likes.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { LikesService } from './likes.service';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
 
 @Controller('likes')
-export class LikesController {}
+@UseGuards(AuthGuard())
+export class LikesController {
+  constructor(private likeService: LikesService) {}
+
+  @Post('/:id')
+  createLike(@Param('id') id: string, @GetUser() user: User): Promise<void> {
+    return this.likeService.likePost(id, user);
+  }
+}
--- whisper/src/likes/likes.module.ts
+++ whisper/src/likes/likes.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { LikesController } from './likes.controller';
 import { LikesService } from './likes.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Like } from './likes.entity';
+import { Post } from 'src/posts/posts.entity';
+import { PostsModule } from 'src/posts/posts.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Like, Post]), PostsModule, AuthModule],
   controllers: [LikesController],
-  providers: [LikesService]
+  providers: [LikesService],
 })
 export class LikesModule {}
